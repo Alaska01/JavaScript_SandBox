489636c2ca03984ae1770865d8c5598376a47578
Alaska01 <aadaniel108@gmail.com>
2022-01-23 23:57:06 +0100

    Flights assignment on string methods

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -3,7 +3,17 @@
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
+//  Personal Solution
+  const individualFlight = (flights.split('+'));
+
+  for (const ind of individualFlight){
+    const msg = ind.split(';');
+    const properMsg = `${msg[0].split('_').join(' ')} From ${msg[1].slice(1,4).toUpperCase()} To ${msg[2].slice(1,4).toUpperCase()} (${msg[3].replace(':', 'h')})`
+    console.log(properMsg.padStart(50));
+  }
+ 
+
+  const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
 const hours = {
   thu: {
     open: 12,
