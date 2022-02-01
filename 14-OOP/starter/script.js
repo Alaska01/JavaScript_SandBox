9293411eb9bd3d2b3c7ee8df2550a9e1fdec0ca1
Alaska01 <aadaniel108@gmail.com>
2022-02-01 15:09:53 +0100

    Getters and Setters Assignment for speed solved

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -23,3 +23,79 @@ car1.accelerate();
 car1.break();
 car1.break();
 // console.log(car1.prototype)
+
+//   Recreating the above with ES6 Classes
+
+// Class Expression
+const CarExp = class {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`Class Expression: The ${this.make} is speeding at ${this.speed} km/h`);
+    }
+
+    break(){
+        this.speed -= 5;
+        console.log(`Class Expression: The ${this.make} is speeding at ${this.speed} km/h`);
+    }
+
+    get speedUS(){
+        return `Getter Class Expression: ${this.speed / 1.6} m/h`
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
+    }
+    
+}
+
+const carExp = new CarExp("BMW", 120);
+carExp.accelerate();
+carExp.accelerate();
+carExp.accelerate();
+carExp.accelerate();
+carExp.break();
+carExp.break();
+console.log(carExp.speedUS);
+carExp.speedUS = 50;
+console.log(carExp.speed)
+
+// Class Declaration
+class CarDec {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`Class Declaration: The ${this.make} is speeding at ${this.speed} km/h`);
+    }
+
+    break(){
+        this.speed -= 5;
+        console.log(`Class Declaration: The ${this.make} is speeding at ${this.speed} km/h`);
+    }
+
+    get speedUS(){
+        return `Getter Class Declaration: ${this.speed / 1.6} m/h`
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
+    }
+
+}
+
+const carDec = new CarDec("BMW", 120);
+carDec.accelerate();
+carDec.accelerate();
+carDec.accelerate();
+carDec.accelerate();
+carDec.break();
+carDec.break();
+console.log(carDec.speedUS);
