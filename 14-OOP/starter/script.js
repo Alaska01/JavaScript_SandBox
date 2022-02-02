8eaba10bd81f422dffbe6c61cb7655393308df9a
Alaska01 <aadaniel108@gmail.com>
2022-02-02 23:34:39 +0100

    Solution to coding challenge 4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -143,3 +143,152 @@ const tesla = new EV3('Tesla', 120, 23)
 console.log(tesla.chargeBattery(85))
 tesla.break()
 tesla.accelerate();
+
+///////////////////////////////////////
+// Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+class Account {
+    // 1) Public fields (instances)
+    locale = navigator.language;
+  
+    // 2) Private fields (instances)
+    #movements = [];
+    #pin;
+  
+    constructor(owner, currency, pin) {
+      this.owner = owner;
+      this.currency = currency;
+      this.#pin = pin;
+  
+      // Protected property
+      // this._movements = [];
+      // this.locale = navigator.language;
+  
+      console.log(`Thanks for opening an account, ${owner}`);
+    }
+  
+    // 3) Public methods
+  
+    // Public interface
+    getMovements() {
+      return this.#movements;
+    }
+  
+    deposit(val) {
+      this.#movements.push(val);
+      return this;
+    }
+  
+    withdraw(val) {
+      this.deposit(-val);
+      return this;
+    }
+  
+    requestLoan(val) {
+    //   if (this.#approveLoan(val)) {
+      if (this._approveLoan(val)) {
+        this.deposit(val);
+        console.log(`Loan approved`);
+        return this;
+      }
+    }
+  
+    static helper() {
+      console.log('Helper');
+    }
+  
+    // 4) Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+      return true;
+    }
+  }
+  
+  const acc1 = new Account('Jonas', 'EUR', 1111);
+  
+  // acc1._movements.push(250);
+  // acc1._movements.push(-140);
+  // acc1.approveLoan(1000);
+  
+  acc1.deposit(250);
+  acc1.withdraw(140);
+  acc1.requestLoan(1000);
+  console.log(acc1.getMovements());
+  console.log(acc1);
+  Account.helper();
+  
+//   console.log(acc1.#movements);
+//   console.log(acc1.#pin);
+//   console.log(acc1.#approveLoan(100));
+  
+  // Chaining
+  acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+  console.log(acc1.getMovements());
+
+//   Challenge 4
+
+class CarCl {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`Class Declaration: The ${this.make} is speeding at ${this.speed} km/h`);
+        // return this;
+    }
+
+    break(){
+        this.speed -= 5;
+        console.log(`Class Declaration: The ${this.make} is speeding at ${this.speed} km/h`);
+        return this;
+    }
+
+    get speedUS(){
+        return `Getter Class Declaration: ${this.speed / 1.6} m/h`
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
+    }
+
+}
+
+class EVCl extends CarCl {
+    #charge;
+    constructor(make, speed, charge){
+        super(make, speed)
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+      }
+    
+      accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(
+          `${this.make} is going at ${this.speed} km/h, with a charge of ${
+            this.#charge
+          }`
+        )
+        
+        return this;
+        ;}
+        
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian)
+// console.log(rivian.#charge)
+rivian.accelerate().accelerate()
