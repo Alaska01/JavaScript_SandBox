02655d854f85e93de2fe10f16bca7b2b6b1eff40
Alaska01 <aadaniel108@gmail.com>
2022-02-07 00:07:41 +0100

    Promise solution added

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -40,13 +40,38 @@ console.log(667)
 // fetch()
 const whereAmI = function(long, lat){
     console.log('Test data')
-    const request = fetch(`https://geocode.xyz/${long},${lat}?geoit=json`).then(function(response){return response.json()})
+    const request = fetch(`https://geocode.xyz/${long},${lat}?geoit=json`)
+    .then(function(response){
+        // console.log(response)
+        if(!response.ok){
+            throw new Error(`Problem with geocoding, ${response.status}`);
+        }
+        return response.json()})
     .then(data => {
-        console.log(data.alt.loc.city)
+        // const city = data.alt.loc.city;
+        const city = data.city;
+        const country = data.country;
+        // if(!city || !country){
+        //     throw new Error('City Not Available Currently')
+        // }
                     // const [country] = data;
                     // ({ country} = data)
-                    console.log(data.country);
+                    // console.log(data)
+                    console.log(`You are in ${city}, ${country}`);
     })
+    .catch(error => console.log(error))
 }
 
 whereAmI(51.50354, -0.12768)
+// whereAmI(52.508, 13.381)
+
+// const whereAmIJonas = function(lat, lng){
+//     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(res => res.json())
+//     .then( data => {
+//         console.log(data);
+//         console.log(`You are in ${data.city}, ${data.country}`)
+//     })
+// }
+
+// whereAmIJonas(52.508, 13.381)
